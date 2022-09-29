b3d1462c149b89ba44b84ce453cd318b31b7c4e9
Queenslay Jema <queenslay.naila@student.moringaschool.com>
2022-09-29 03:15:33 +0300

    completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,22 @@
 import React from "react";
 import { actors } from "../data";
-
+const actorlist = actors.map((act)=>{
+  return (
+    <div key={act.name}>
+      <h2>{act.name}</h2>
+      <ul>
+        {act.movies.map((m)=>{
+          return <li key={m}>{m}</li>
+        })}
+      </ul>
+    </div>
+  )
+})
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (<div>
+    <h1>Actors Page</h1>
+    {actorlist}
+  </div>);
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,25 @@
 import React from "react";
 import { movies } from "../data";
 
+const movielist = movies.map((movie)=>{
+  return (
+    <div key={movie.title}>
+      <h2>{movie.title}</h2>
+      <p>Runtime: {movie.time} min.</p>
+      <ul key={movie.title}>
+        {movie.genres.map((genre)=>{
+          return <li key={genre}>{genre}</li>
+        })}
+      </ul>
+    </div>
+
+  )
+})
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return( <div>
+    <h1>Movies Page</h1>
+     {movielist}
+  </div>);
 }
 
 export default Movies;
